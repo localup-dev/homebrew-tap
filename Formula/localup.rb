17848608c3557b8e0ab1eb8da7e47de2a4cab286
David Viejo <dviejo@kungfusoftware.es>
2025-12-27 16:42:11 +0100

    Update localup to v0.0.1-beta40

--- Formula/localup.rb
+++ Formula/localup.rb
@@ -1,26 +1,26 @@
 class Localup < Formula
   desc "LocalUp - Local development environment setup and management"
   homepage "https://github.com/localup-dev/localup"
-  version "0.0.1-beta35"
+  version "0.0.1-beta40"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta35/localup-macos-arm64.tar.gz"
-      sha256 "769627aa74fa573757cac39efc8ecd25525d1eb09318265d871a8c9f806310ea"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-macos-arm64.tar.gz"
+      sha256 "19f816a5f1d628e2159b721dc5187aedfe709fa91d321cdd0b03d3979f3c43f2"
     else
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta35/localup-macos-amd64.tar.gz"
-      sha256 "7fc3d0adb51ca7e0effe0e0b1bc80b0df9e1b4d38700280e38862b33bbd67c43"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-macos-amd64.tar.gz"
+      sha256 "a1814c5fc76e853298403780f72633bc3f698875e204d3f6fb525ab852dbf41e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta35/localup-linux-arm64.tar.gz"
-      sha256 "a442fa99cab6b20e8ff255fd6530a17f3bd55a9ece16fdd92043cfbad342b026"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-linux-arm64.tar.gz"
+      sha256 "61a50229b208007f81b9328ae7d5e4209a4c1282c5b3cc8e9a958c82b13d2a2e"
     elsif Hardware::CPU.intel?
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta35/localup-linux-amd64.tar.gz"
-      sha256 "9e92d2785cd462635afffe7ec6f239bee411acd7f94f838f51ab1c7d2dc74336"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-linux-amd64.tar.gz"
+      sha256 "bbbf4b3c319e55fa67d24d49da4ee82d478c5f2a6d692b8db7cba07bd4bb9d90"
     end
   end
 
