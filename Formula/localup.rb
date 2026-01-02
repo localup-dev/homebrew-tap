e3273c35703f30a1df7f5979cf435427d69549e9
David Viejo <dviejo@kungfusoftware.es>
2026-01-03 00:18:31 +0100

    Update localup to v0.0.1-beta46
    - Bump version from 0.0.1-beta40 to 0.0.1-beta46
    - Update download URLs and SHA256 checksums for macOS and Linux binaries
    
    Signed-off-by: David Viejo <dviejo@kungfusoftware.es>

--- Formula/localup.rb
+++ Formula/localup.rb
@@ -1,26 +1,26 @@
 class Localup < Formula
   desc "LocalUp - Local development environment setup and management"
   homepage "https://github.com/localup-dev/localup"
-  version "0.0.1-beta40"
+  version "0.0.1-beta46"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-macos-arm64.tar.gz"
-      sha256 "19f816a5f1d628e2159b721dc5187aedfe709fa91d321cdd0b03d3979f3c43f2"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta46/localup-macos-arm64.tar.gz"
+      sha256 "748743de36dcec0096344610283d760ff9e8f9072c27c0c133e90fe2c470ab34"
     else
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-macos-amd64.tar.gz"
-      sha256 "a1814c5fc76e853298403780f72633bc3f698875e204d3f6fb525ab852dbf41e"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta46/localup-macos-amd64.tar.gz"
+      sha256 "f4f78330f3b07f96a41f101cb28a4bc58b119326943680f9b50dbc9a9d0ad204"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-linux-arm64.tar.gz"
-      sha256 "61a50229b208007f81b9328ae7d5e4209a4c1282c5b3cc8e9a958c82b13d2a2e"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta46/localup-linux-arm64.tar.gz"
+      sha256 "7328b59fdbd23ad94bfe6c273c1656a3ed5b50884f86197875ae1c83029af432"
     elsif Hardware::CPU.intel?
-      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta40/localup-linux-amd64.tar.gz"
-      sha256 "bbbf4b3c319e55fa67d24d49da4ee82d478c5f2a6d692b8db7cba07bd4bb9d90"
+      url "https://github.com/localup-dev/localup/releases/download/v0.0.1-beta46/localup-linux-amd64.tar.gz"
+      sha256 "04dd8ac397d1d2a1e699be174d07c901322d6bde0afa97e81e5d89af6b527b84"
     end
   end
 
